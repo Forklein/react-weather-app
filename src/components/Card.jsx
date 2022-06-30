1563e4e664f3c281feeaf52a14621d066d1cf286
Giuseppe <giuseppe.pisani@webora.it>
2022-06-30 12:51:04 +0200

    Add card and logic for rendering

---
+++ src/components/Card.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Card = (props) => {
+  const { location } = props;
+  if (location === null) return null;
+
+  return (
+    <div className="card mx-auto" style={{ width: "18rem" }}>
+      {/* <img className="card-img-top" src="..." alt="Card image cap" /> */}
+      <div className="card-header">
+        <h2>{location.name}</h2>
+      </div>
+      <div className="card-body">
+        {/* <h5 className="card-title my-2">{location.name}</h5> */}
+        <div className="card-text">
+          <p>
+            Temperatura: <strong>{location.main.temp}°C</strong>
+          </p>
+          <p>
+            Temperatura Minima: <strong>{location.main.temp_min}°C</strong>
+          </p>
+          <p>
+            Temperatura Massima: <strong>{location.main.temp_max}°C</strong>
+          </p>
+        </div>
+      </div>
+      <div className="card-footer">
+        <p>{location.weather[0].description.toUpperCase()}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Card from "./Card";
 
 const Weather = () => {
   const [lat, setLat] = useState(null);
   const [long, setLong] = useState(null);
-  const [location, setLocation] = useState([]);
+  const [location, setLocation] = useState(null);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(function (position) {
       setLat(position.coords.latitude);
       setLong(position.coords.longitude);
     });
-
     // console.log("Latitude is:", lat);
     // console.log("Longitude is:", long);
+    // console.log(location);
   }, [lat, long]);
 
   const getData = async () => {
@@ -22,7 +23,8 @@ const Weather = () => {
         `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
       );
       setLocation(data);
-      console.log(location);
+      // console.log(data);
+      // console.log(location);
     } catch (error) {
       console.log(error);
     }
@@ -31,7 +33,10 @@ const Weather = () => {
   return (
     <div className="weather">
       <h1>Weather</h1>
-      <button></button>
+      <button className="btn btn-primary mb-3" onClick={getData}>
+        Click for info weather
+      </button>
+      <Card location={location} />
     </div>
   );
 };
