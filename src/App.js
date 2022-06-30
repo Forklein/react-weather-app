0b46b1ea586cd7dd533934a387910a282234b5d6
Giuseppe <giuseppe.pisani@webora.it>
2022-06-30 17:01:25 +0200

    Add shadow effect

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Weather from './components/Weather';
 function App() {
   return (
     <div className="container vh-100 text-center d-flex flex-column align-items-center justify-content-center">
-      <div className="App col-xs-4 col-md-6 col-lg-6 mx-auto">
+      <div className="App col-xs-4 col-md-6 col-lg-6 mx-auto shadow">
         <Weather />
       </div>
     </div>
